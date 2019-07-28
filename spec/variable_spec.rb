1d37b29a629a8e2fd74afd92a320c3cfb8ee005e
Richard Burd <richardburdx@yahoo.com>
2019-07-28 00:27:37 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -8,3 +8,13 @@ describe "./variable.rb" do
   end
 
 end
+
+describe "./burdtest.rb" do
+
+  it "defined a local variable called burd_variable and set it to say something nice about Iraq" do
+    nicety = get_variable_from_file('./burdtest.rb', "burd_variable")
+
+    expect(nicety).to eq("Duhok is a fine city")
+  end
+
+end
